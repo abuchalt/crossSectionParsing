4962f818740b030554b9ab1e8ff6f89d9c71c584
bucky2503 <bucky3052@gmail.com>
2025-07-08 15:37:50 -0400

    plot n+gamma for multiple elements

--- plotAtt.py
+++ plotAtt.py
@@ -20,10 +20,10 @@ Erange = (0.05, 15) # [MeV]
 # Elements of Interest
 # ------------------------------------------------------------------------------
 myList = [
+    {'symbol': 'W', 'density': 19.3,'mass': 183.84}, # Tungsten
     {'symbol': 'Fe', 'density': 7.87,'mass': 55.845}, # BCC Iron
-    {'symbol': 'Al', 'density': 2.71,'mass': 26.981539}, # Aluminum
     {'symbol': 'Ti', 'density': 4.51,'mass': 47.867}, # Titanium
-    {'symbol': 'W', 'density': 19.3,'mass': 183.84} # Tungsten
+    {'symbol': 'Al', 'density': 2.71,'mass': 26.981539} # Aluminum
 ] # Atomic Symbol, Density [g/cc], Molar Mass [Da]
 
 
@@ -38,13 +38,9 @@ for element in myList:
     ρ = element['density']
     M = element['mass']
     XCOMdir = f'CrossSectionData{os.path.sep}'+symbol+f'_XCOM.txt'
-    EXFORdir = f'CrossSectionData{os.path.sep}'+symbol+f'0_EXFOR.csv'
-
-    # Calculated Properties
-    N = xsm.numberDensity(ρ, M) # [cm^-3]
 
     # Debug
-    print(symbol)
+    # print(symbol)
 
     # Plot
     MuData = xsm.parseXCOM(XCOMdir, ρ)
@@ -67,24 +63,43 @@ plt.legend()
 plt.tight_layout()
 plt.show()
 
-quit()
+
 
 # Neutrons
-FeSigma = xsm.parseEXFOR(f'CrossSectionData{os.path.sep}Fe0_EXFOR.csv', FeNumberDensity)
-# Plot
-xdata = xsm.neutronWavelength(FeSigma[:,0]) # [eV] -> [A]
-ydata = FeSigma[:,1] # [/cm]
-plt.plot(xdata, ydata)
-# Formatting
-xmin = 0.1
-xmax = 10
-plt.xlim(λrange)
-visible_y = ydata[(xdata >= λrange[0]) & (xdata <= λrange[1])]
+# ------------------------------------------------------------------------------
+visible_y = np.array([])
+for element in myList:
+
+    # Extract Dict
+    symbol = element['symbol']
+    ρ = element['density']
+    M = element['mass']
+    EXFORdir = f'CrossSectionData{os.path.sep}'+symbol+f'0_EXFOR.csv'
+
+    # Calculated Properties
+    N = xsm.numberDensity(ρ, M) # [cm^-3]
+
+    # Debug
+    # print(symbol)
+
+    # Plot
+    SigmaData = xsm.parseEXFOR(EXFORdir, N)
+    xdata = xsm.neutronWavelength(SigmaData[:,0]) # [eV] -> [A]
+    ydata = SigmaData[:,1] # [/cm]
+    plt.plot(xdata, ydata, label=symbol)
+
+    # Formatting
+    plt.xlim(λrange)
+    visible_y = np.append(visible_y, ydata[(xdata >= λrange[0]) & (xdata <= λrange[1])])
+
+# Formatting   
 if len(visible_y) > 0: # Set y-limits based on visible data
     plt.ylim(np.min(visible_y)/margin, np.max(visible_y)*margin)
-# plt.xscale('log')
+plt.xscale('log')
 plt.yscale('log')
 plt.ylabel('Linear Attenuation Coefficient [/cm]')
 plt.xlabel('Neutron Wavelength [Angstrom]')
+plt.title('Neutron Absorption')
+plt.legend()
 plt.tight_layout()
 plt.show()
