a38711a84060bbff07001214bef3f9385208cad1
Adam Buchalter <abuchalt@vols.utk.edu>
2025-07-08 12:44:54 -0400

    vectorize

--- plotAtt.py
+++ plotAtt.py
@@ -2,8 +2,6 @@ import XSectModule as xsm
 import os
 import matplotlib.pyplot as plt
 import numpy as np
-# Iron (for various phases: Example BCC) and  Aluminum, and Titanium, and Tungsten
-# 0.1 to 15 Angstroms
 
 # Universal Constants
 # ------------------------------------------------------------------------------
@@ -19,31 +17,57 @@ Erange = (0.05, 15) # [MeV]
 
 
 
-# BCC Iron
+# Elements of Interest
 # ------------------------------------------------------------------------------
+myList = [
+    {'symbol': 'Fe', 'density': 7.87,'mass': 55.845}, # BCC Iron
+    {'symbol': 'Al', 'density': 2.71,'mass': 26.981539}, # Aluminum
+    {'symbol': 'Ti', 'density': 4.51,'mass': 47.867}, # Titanium
+    {'symbol': 'W', 'density': 19.3,'mass': 183.84} # Tungsten
+] # Atomic Symbol, Density [g/cc], Molar Mass [Da]
 
-# Material Properties
-FeDensityBCC = 7.87 # [g/cc]
-FeA = 55.845 # [Da]
-FeNumberDensity = xsm.numberDensity(FeDensityBCC, FeA) # [cm^-3]
+
 
 # Photons
-FeMu = xsm.parseXCOM(f'CrossSectionData{os.path.sep}Fe_XCOM.txt', FeDensityBCC)
-# Plot
-xdata = FeMu[:,0]/1000000 # [eV] -> [MeV]
-ydata = FeMu[:,1] # [/cm]
-plt.plot(xdata, ydata)
-# Formatting
-plt.xlim(Erange)
-visible_y = ydata[(xdata >= Erange[0]) & (xdata <= Erange[1])]
+# ------------------------------------------------------------------------------
+visible_y = np.array([])
+for element in myList:
+
+    # Extract Dict
+    symbol = element['symbol']
+    ρ = element['density']
+    M = element['mass']
+    XCOMdir = f'CrossSectionData{os.path.sep}'+symbol+f'_XCOM.txt'
+    EXFORdir = f'CrossSectionData{os.path.sep}'+symbol+f'0_EXFOR.csv'
+
+    # Calculated Properties
+    N = xsm.numberDensity(ρ, M) # [cm^-3]
+
+    # Debug
+    print(symbol)
+
+    # Plot
+    MuData = xsm.parseXCOM(XCOMdir, ρ)
+    xdata = MuData[:,0]/1000000 # [eV] -> [MeV]
+    ydata = MuData[:,1] # [/cm]
+    plt.plot(xdata, ydata, label=symbol)
+
+    # Formatting
+    plt.xlim(Erange)
+    visible_y = np.append(visible_y, ydata[(xdata >= Erange[0]) & (xdata <= Erange[1])])
+
+# Formatting   
 if len(visible_y) > 0: # Set y-limits based on visible data
     plt.ylim(np.min(visible_y)/margin, np.max(visible_y)*margin)
 plt.xscale('log')
 plt.yscale('log')
 plt.ylabel('Linear Attenuation Coefficient [/cm]')
 plt.xlabel('Photon Energy [MeV]')
+plt.legend()
 plt.tight_layout()
 plt.show()
+
+quit()
 
 # Neutrons
 FeSigma = xsm.parseEXFOR(f'CrossSectionData{os.path.sep}Fe0_EXFOR.csv', FeNumberDensity)
